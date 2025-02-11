c3cfee4cd2a673688b348b23cd3cd859807005f7
Natsu ☕✨ <75032462+NatsuPotato@users.noreply.github.com>
2025-02-11 13:40:52 -0500

    Update index.php

--- index.php
+++ index.php
@@ -8,11 +8,14 @@
 
 		h1 { margin: 0; }
 
+		a { text-decoration: none; }
+		a:hover { text-decoration: underline; }
+
 		header, aside, main { padding: 1em; }
 
-		header { background: #159; color: white; grid-column: 1 / span 2; text-align: center; }
-		main { background: #cef; }
-		aside { background: #fff; border-right: 1px solid black; }
+		header { background: #159; color: white; grid-column: 1 / span 2; }
+		main { background: #bdf; }
+		aside { background: #def; border-right: 1px solid black; }
 	</style>
 </head>
 <body>
@@ -21,7 +24,7 @@
 	<!-- layout inspired by https://webdesignerdepot-wp.s3.us-east-2.amazonaws.com/2023/11/27135129/01-current-DA-homepage.jpg -->
 
 	<header>
-		<h1>[<a href="/">localbooru</a>]</h1>
+		<h1><a href="/" style="color: white;">localbooru</a></h1>
 	</header>
 
 	<aside>
@@ -33,7 +36,7 @@
 
 				if ($file != "." && $file != ".." && is_dir("./boards/$file")) {
 
-					echo "<br><br><a href='$file'>/$file</a>";
+					echo "<br><br><a href='$file'>$file</a>";
 				}
 			}
 		?>
@@ -70,6 +73,7 @@
 			if ($uri_elements[0] == "/") {
 
 				// home
+				echo "<h2>Welcome to localbooru!</h2><p>We are currently serving [num] images.</p>";
 
 			} else if (count($uri_elements) == 1) {
 
